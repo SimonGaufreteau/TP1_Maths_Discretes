e215dc0fe01ddee8462a1031db200224da6d93ad
SimonGaufreteau <simon.gaufreteau@gmail.com>
2020-03-27 18:49:22 +0100

    Questions 1 à 5.

---
+++ src/Algorithms.java
@@ -0,0 +1,107 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class Algorithms {
+
+	/**
+	 * Implémentation de l'algorithme d'Euclide étendu vu en cours.
+	 * @param a int
+	 * @param b int
+	 * @return Un tableau d'entier : 0 --> pgcd de a et b / 1 et 2 --> coefficients de Bézout u et v tels que a*u+b*v=pgcd(a,b).
+	 */
+	public static int[] euclideEtendu(int a,int b){
+		return euclideEtenduRecursif(a,b);
+	}
+
+	private static int[] euclideEtenduRecursif(int a,int b) {
+		int[] result = new int[3];
+		if (b == 0) {
+			result[0] = a;
+			result[1] = 1;
+		}
+		else{
+			int[] temp = euclideEtenduRecursif(b,a%b);
+			result[0]=temp[0];
+			result[1]=temp[2];
+			result[2]=temp[1]-(a/b)*temp[2];
+		}
+		return result;
+	}
+
+	/**
+	 * Affiche le résultat de l'éxecution de l'algorithme d'Euclide étendu donné en paramètre.
+	 * @param a int
+	 * @param b int
+	 * @param result Resultat de l'éxecution de l'algorithme d'Euclide étendu définit dans "euclideEtendu" avec les paramètres a et b.
+	 */
+	public static void printResultEuclideEtendu(int a,int b,int[] result){
+		int resultMult=a*result[1]+b*result[2];
+		System.out.println(String.format("Résultat de l'algo euclide étendu. Pgcd de %d et %d : %d. %d*%d + %d*%d = %d.",a,b,result[0],a,result[1],b,result[2],resultMult));
+	}
+
+
+	/**
+	 * Implémentation de l'algorithme de l'exponentiation modulaire : a^k mod n.
+	 */
+	public static int ExpMod(int a, int k, int n) {
+		BigInteger bigA = BigInteger.valueOf(a);
+		BigInteger p;
+		for (p=BigInteger.ONE; k>0; k/=2) {
+			if(k%2 != 0)
+				p = (p.multiply(bigA)).mod(BigInteger.valueOf(n)) ;
+			bigA = (bigA.multiply(bigA)).mod(BigInteger.valueOf(n)) ;
+		}
+		return p.intValue();
+	}
+
+	/**
+	 * Test de Fermat sur n
+	 */
+	public static boolean testFermat(int n){
+		int test =  ExpMod(2,n-1,n);
+		return test%n==1;
+	}
+
+	/**
+	 * Test de primalité naive sur un entier n.
+	 */
+	public static boolean testPrimaliteNaif(int n){
+		for(int i=2;i<=Math.sqrt(n);i++){
+			if(n%i==0){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Calcule le nombre d'entiers premiers entre 2^i et 2^(i+1)
+	 */
+	public static int nombrePremiersNaif(int i){
+		int compteur = 0;
+		for(int j=(int)Math.pow(2,i);j<(int)Math.pow(2,i+1);j++){
+			compteur+=testPrimaliteNaif(j)?1:0;
+		}
+		return compteur;
+	}
+
+	/**
+	 * Calcule le nombre d'entiers premiers entre 2^i et 2^(i+1) avec le test de Fermat. Pour la méthode naive mais exacte, utiliser nombrePremiersNaif
+	 */
+	public static int nombrePremiersFermat(int i){
+		int compteur = 0;
+		for(int j=(int)Math.pow(2,i);j<(int)Math.pow(2,i+1);j++){
+			compteur+=testFermat(j)?1:0;
+		}
+		return compteur;
+	}
+
+	public static ArrayList<Integer> diffPremiersFermat(int i){
+		ArrayList<Integer> tab = new ArrayList<>();
+		for(int j=(int)Math.pow(2,i);j<(int)Math.pow(2,i+1);j++){
+			if(testFermat(j) && !testPrimaliteNaif(j))
+				tab.add(j);
+		}
+		return tab;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Random;
+
 public class Main {
 	public static void main(String[] args) {
-		
+
+		/*int a=120;
+		int b=12;
+		int[] tab= Algorithms.euclideEtendu(a,b);
+		Algorithms.printResultEuclideEtendu(a,b,tab);
+
+		int p = Algorithms.ExpMod(4,13,497);
+		System.out.println("Résultat de l'exp modulaire de "+4+"^"+13+" mod "+497+" = "+p);
+
+		Random r = new Random();
+		int borne = 65536;
+		for(int i=0;i<100;i++){
+			int alea = r.nextInt(borne)+borne;
+			boolean test = Algorithms.testPrimaliteNaif(alea);
+			if(test) {
+				System.out.println("Résultat du test de Fermat sur " + alea + " : " + Algorithms.testFermat(alea));
+				System.out.println("Résultat du test de primalité naïve sur " + alea + " : " + Algorithms.testPrimaliteNaif(alea) + "\n");
+			}
+		}
+*/
+		int borneNbPremiers = 10;
+		for(int i=0;i<=borneNbPremiers;i++){
+			int borneInf = (int)Math.pow(2,i);
+			int borneSup = (int)Math.pow(2,i+1);
+			int nombrePremiersResultat = Algorithms.nombrePremiersNaif(i);
+			int nombrePermiersFermat = Algorithms.nombrePremiersFermat(i);
+			System.out.println(String.format("Nombre de premiers pour i=%d (entre %d et %d) : %d",i,borneInf,borneSup,nombrePremiersResultat));
+			System.out.println(String.format("Proportion de nombres premiers : %d %%",nombrePremiersResultat*100/(borneSup-borneInf)));
+			System.out.println(String.format("Nombre de premiers pour i=%d avec Fermat (entre %d et %d) : %d",i,borneInf,borneSup,nombrePermiersFermat));
+			System.out.println(String.format("Nombre de non premiers détectés avec Fermat : %d",nombrePermiersFermat-nombrePremiersResultat));
+			System.out.println(String.format("Taux d'erreur de Fermat : %f %%",(double)((nombrePermiersFermat-nombrePremiersResultat)*100)/(borneSup-borneInf)));
+			System.out.println("Faux positifs : "+Algorithms.diffPremiersFermat(i));
+			System.out.println("\n");
+		}
+
 	}
+
 }
